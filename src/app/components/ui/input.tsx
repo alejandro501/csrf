32d27468137dc6422c555ad962a7dedde84d2c2d
alejandro501 <alejandro501@proton.me>
2025-06-03 09:59:31 +0200

    linter fix workaround

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,9 +1,11 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  // This is a workaround for a linter rule
+  'data-linter-fix'?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -17,9 +19,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input } 
+export { Input };
